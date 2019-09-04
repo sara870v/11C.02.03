78da927196b8978340355d2d3758dac450ae7424
Sarah Davidsen <sarahdavidsen@Sarahs-MacBook-Pro-2.local>
2019-09-04 22:07:42 +0200

    finished as good as possible

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ let allStudents = [];
 
 let housebtn = document.querySelector(".housebtn");
 let housecontent = document.querySelector(".housedropdown");
+let sortbtn = document.querySelector(".sortbtn");
+let sortcontent = document.querySelector(".sortdropdown");
+
+let house = "All";
 
 housebtn.addEventListener("click", dropHouse);
+sortbtn.addEventListener("click", dropSort);
 
 function dropHouse() {
-  // display the content of different houses
+  // display the content of different houses from the filter menu
   housecontent.classList.toggle("show");
 }
-let house = "All";
+
+function dropSort() {
+  // display the content of different content from the sort menu
+  sortcontent.classList.toggle("show");
+}
 
 document.addEventListener("DOMContentLoaded", start);
 
@@ -36,19 +45,37 @@ function start() {
                   <h3>${student.house}</h3
               </div>`;
       }
+
+      document.querySelectorAll(".student").forEach(stdnt => {
+        stdnt.addEventListener("click", open);
+      });
+      function open() {
+        //function that opens the popup, shows information and closes it again
+        console.log(student);
+        document.querySelector("#indhold").innerHTML = `
+                            <div class="students">
+                                <h2>${student.fullname}</h2>
+                                <p>${student.house}</p>
+                                </div>
+                            `;
+        document.querySelector("#popup").style.display = "block";
+
+        document.querySelector("#luk button").addEventListener("click", () => {
+          document.querySelector("#popup").style.display = "none";
+        });
+      }
     });
+
+    document.querySelectorAll(".filter").forEach(elm => {
+      elm.addEventListener("click", showHouse);
+    });
+
+    function showHouse() {
+      // show what house is specific to the content you filter
+      house = this.textContent;
+
+      showStudent();
+    }
   }
-
-  document.querySelectorAll(".filter").forEach(elm => {
-    elm.addEventListener("click", showHouse);
-  });
-
-  function showHouse() {
-    // show what house is specific to the content you filter
-    house = this.textContent;
-
-    showStudent();
-  }
-
   getJson();
 }
